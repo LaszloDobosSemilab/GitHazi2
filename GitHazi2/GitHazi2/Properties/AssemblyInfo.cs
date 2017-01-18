21b1a3c7ea1ba05f876c137555a3e90579b0ab3c
Gabor Varnagy <gabor.varnagy@semilab.hu>
2017-01-18 15:19:27 +0100

    important copyright message

--- GitHazi2/GitHazi2/Properties/AssemblyInfo.cs
+++ GitHazi2/GitHazi2/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GitHazi2")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © DL VG 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

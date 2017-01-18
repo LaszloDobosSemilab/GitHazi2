6a43eae98824d699d80ed56d00097add049811e6
Gabor Varnagy <gabor.varnagy@semilab.hu>
2017-01-18 16:00:51 +0100

    Finishing touches.

--- GitHazi2/GitHazi2/Program.cs
+++ GitHazi2/GitHazi2/Program.cs
@@ -11,7 +11,7 @@ namespace GitHazi2
         static void Main(string[] args)
         {
         }
-        static void B() { }
+        static void B() { Console.WriteLine("B"); }
 
         static void QMain(string[] args)
         {
